0da5cc779099dd48d183f3de7f46eb215ccdaef3
Sergiofrrg92 <sergiofrrg@gmail.com>
2020-10-20 23:01:40 +0100

    Adding tablelayout adding buttons setting style to buttons added layout info to buttons: weight and width=0dp adding constrains Adding Calculator textview adding number textview

--- app/src/main/java/com/example/calculatorapp/MainActivity.java
+++ app/src/main/java/com/example/calculatorapp/MainActivity.java
@@ -3,12 +3,18 @@ package com.example.calculatorapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    static int numberOnScreen=0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
     }
 }
